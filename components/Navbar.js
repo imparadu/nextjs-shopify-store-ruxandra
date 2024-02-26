7b8f5d19b6e956e9968c7e079ba0f6b24960eedd
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-02-26 11:26:27 +0200

    lucru la css

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,23 +3,26 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <div className="navigation">
-      <ul>
-        <li>
+      <ul className="p-0 mt-1.5 list-none inline-flex">
+        <li className="px-3 items-center">
           <Link href="/portfolio">Portfolio</Link>
         </li>
-        <li>
+        <li className="px-3 items-center">
           <Link href="/sketchbook">Sketchbook</Link>
         </li>
-        <li>
+        <li className="px-3 items-center">
           <a
             href="https://www.instagram.com/chocolatechiphelmet/ "
             target="blank"
+            className="text-17rem"
           >
             Instagram
           </a>
         </li>
-        <li>
+        <li className="px-3 items-center">
           <Link href="/contact">Contact</Link>
+          {/*   font-size: 1.7rem;
+  color: black; */}
         </li>
       </ul>
     </div>
